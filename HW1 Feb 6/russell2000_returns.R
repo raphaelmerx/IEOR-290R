c624b7dd406d484161cb96800d0757ab5515c42d
raphaelmerx <raphael.merx@berkeley.edu>
2014-02-04 15:47:30 -0800

    initial version

---
+++ HW1 Feb 6/russell2000_returns.R
@@ -0,0 +1,51 @@
+russell2000 = read.csv("russell2000_daily.csv")
+
+calculateDailyReturns <- function(Close){
+  Return <- vector()
+  for (i in 1:(length(Close)-1)) {
+    Return[i] <- (Close[i] - Close[i+1]) / Close[i+1]
+  }
+  Return
+}
+
+calculateWeeklyReturns <- function(Close){
+  weeklyReturns <- numeric()
+  i <- 1
+  while(! is.na(Close[i+5])) {
+    thisReturn = (Close[i]-Close[i+5]) / Close[i+5]
+    weeklyReturns <- c(weeklyReturns, thisReturn)
+    i <- i+5
+  }
+  weeklyReturns
+}
+
+# approx 21 = 252/12 trading days in a month
+calculateMonthlyReturns <- function(Close){
+  monthlyReturns <- numeric()
+  i <- 1
+  while(! is.na(Close[i+21])) {
+    thisReturn = (Close[i]-Close[i+21]) / Close[i+21]
+    monthlyReturns <- c(monthlyReturns, thisReturn)
+    i <- i+21
+  }
+  monthlyReturns
+}
+
+calculateYearlyReturns <- function(Close){
+  yearlyReturns <- numeric()
+  i <- 1
+  while(! is.na(Close[i+252])) {
+    thisReturn = (Close[i]-Close[i+252]) / Close[i+252]
+    yearlyReturns <- c(yearlyReturns, thisReturn)
+    i <- i+252
+  }
+  yearlyReturns
+}
+
+
+dailyReturns <- calculateDailyReturns(russell2000$Close)
+weeklyReturns <- calculateWeeklyReturns(russell2000$Close)
+monthlyReturns <- calculateMonthlyReturns(russell2000$Close)
+yearlyReturns <- calculateYearlyReturns(russell2000$Close)
+
+hist(yearlyReturns, xlim = range(-0.5,0.7), breaks=10)
