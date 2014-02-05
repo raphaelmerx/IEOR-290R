5e9c86d42c6fe22cd926e8c64ff3fb62ab3a0aa6
raphaelmerx <raphael.merx@berkeley.edu>
2014-02-04 16:33:41 -0800

    new returns function

--- HW1 Feb 6/sp500_returns.R
+++ HW1 Feb 6/sp500_returns.R
@@ -1,51 +1,21 @@
 sp500 = read.csv("sp500_daily.csv")
 
-calculateDailyReturns <- function(Close){
-  Return <- vector()
-  for (i in 1:(length(Close)-1)) {
-    Return[i] <- (Close[i] - Close[i+1]) / Close[i+1]
-  }
-  Return
-}
-
-calculateWeeklyReturns <- function(Close){
-  weeklyReturns <- numeric()
+calculateReturns <- function(price,period){
+  Returns <- numeric()
   i <- 1
-  while(! is.na(Close[i+5])) {
-    thisReturn = (Close[i]-Close[i+5]) / Close[i+5]
-    weeklyReturns <- c(weeklyReturns, thisReturn)
-    i <- i+5
+  while(! is.na(price[i+period])) {
+    thisReturn = (price[i]-price[i+period]) / price[i+period]
+    Returns <- c(Returns, thisReturn)
+    i <- i+period
   }
-  weeklyReturns
+  Returns
 }
 
 # approx 21 = 252/12 trading days in a month
-calculateMonthlyReturns <- function(Close){
-  monthlyReturns <- numeric()
-  i <- 1
-  while(! is.na(Close[i+21])) {
-    thisReturn = (Close[i]-Close[i+21]) / Close[i+21]
-    monthlyReturns <- c(monthlyReturns, thisReturn)
-    i <- i+21
-  }
-  monthlyReturns
-}
 
-calculateYearlyReturns <- function(Close){
-  yearlyReturns <- numeric()
-  i <- 1
-  while(! is.na(Close[i+252])) {
-    thisReturn = (Close[i]-Close[i+252]) / Close[i+252]
-    yearlyReturns <- c(yearlyReturns, thisReturn)
-    i <- i+252
-  }
-  yearlyReturns
-}
-
-
-dailyReturns <- calculateDailyReturns(sp500$Close)
-weeklyReturns <- calculateWeeklyReturns(sp500$Close)
-monthlyReturns <- calculateMonthlyReturns(sp500$Close)
-yearlyReturns <- calculateYearlyReturns(sp500$Close)
+dailyReturns <- calculateReturns(sp500$Close,1)
+weeklyReturns <- calculateReturns(sp500$Close,5)
+monthlyReturns <- calculateReturns(sp500$Close,21)
+yearlyReturns <- calculateReturns(sp500$Close,252)
 
 hist(yearlyReturns, xlim = range(-0.5,0.7), breaks=10)
